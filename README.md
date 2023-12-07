9c68cffc0b43764047dc8e290f9bb8b6bd203c89
AdityaPourush <adityapourush11@outlook.com>
2023-12-07 18:02:18 +0530

    part III done

--- README.md
+++
@@ -1,1 +0,0 @@
-# Think-with-React-Part-III
---
+++ first/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// ReactDOM.render("Hello", document.getElementById("root"));
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello Kalvium</h1>);
